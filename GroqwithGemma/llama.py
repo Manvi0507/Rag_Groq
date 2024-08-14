d4df107c57357a04f80921101ccedaef6edf435b
Manvi0507 <manvisharmagn@gmail.com>
2024-08-15 00:29:05 +0530

    Update llama.py

--- GroqwithGemma/llama.py
+++ GroqwithGemma/llama.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import os
+import requests
+import urllib
 from langchain_groq import ChatGroq
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
